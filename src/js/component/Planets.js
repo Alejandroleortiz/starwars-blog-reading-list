2bd2c517912331787f6df2c41760ee4d17d4a721
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-03-14 22:25:16 +0000

    culminado planets

---
+++ src/js/component/Planets.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { checkPropTypes } from "prop-types";
+import { Context } from "../store/appContext";
+import { useContext } from "react";
+
+function Planets(props) {
+  const { store, actions } = useContext(Context);
+
+  const handleAddFavorite = () => {
+    actions.addFavorite(props.name);
+  };
+
+  return (
+    <div className="card my-4" style={{ width: "18rem" }}>
+      <img
+        src="https://dummyimage.com/400x200/cccccc/f2f2f2.png"
+        className="card-img-top"
+        alt="..."
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props?.name}</h5>
+        <p className="card-text my-0">Population: {props?.population}</p>
+        <p className="card-text my-0">Terrain: {props?.terrain}</p>
+        <div className="d-flex justify-content-between mt-4">
+          <Link to="/demo">
+            <a href="#" className="btn btn-outline-primary">
+              Learn More!
+            </a>
+          </Link>
+          <button
+            type="button"
+            className="btn btn-outline-warning"
+            disabled={store.favorites.includes(props.name)}
+            onClick={handleAddFavorite}
+          >
+            <FaHeart />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+checkPropTypes = {
+  name: checkPropTypes.string,
+  population: checkPropTypes.string,
+  terrain: checkPropTypes.string,
+};
+
+export default Planets;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../../styles/home.css";
 import Card from "../component/card";
+import Planets from "../component/Planets";
 import { Context } from "../store/appContext";
 import { useContext } from "react";
+
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -17,6 +19,19 @@ export const Home = () => {
         store.characters?.results.map((character)=>{
           console.log(character);
           return ( <div className="col-md-3"><Card  {...character.properties}/></div> )
+        })
+      }
+    </div>
+  </div>
+
+  <div className="container">
+  <h1 className="text-danger">Planets</h1>
+    <div className="row">
+      {
+        !!store.planets &&
+        store.planets?.results.map((planet)=>{
+          console.log(planet);
+          return ( <div className="col-md-3"><Planets  {...planet.properties}/></div> )
         })
       }
     </div>
